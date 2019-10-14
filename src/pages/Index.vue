e7b08697fce8453e54f0ac51cd95aa755afb1804
mimeonline <michael.meierhoff@gmail.com>
2019-10-14 23:27:40 +0200

    Custimize Layout and add first Cars for Timeline.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,72 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page class="">
+    <div class="row justify-center ">
+      <div class="q-pa-lg col-auto ">
+        <q-card class="my-card">
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label>Spaghetti Bolognese</q-item-label>
+              <q-item-label caption>Subhead</q-item-label>
+            </q-item-section>
+          </q-item>
+          <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+
+          <q-card-section>
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+
+        <q-card class="my-card q-mt-lg">
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label>Schweinebraten mit Knödel</q-item-label>
+              <q-item-label caption>Subhead</q-item-label>
+            </q-item-section>
+          </q-item>
+          <img src="https://cdn.quasar.dev/img/parallax2.jpg">
+          <q-card-section>
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+          <q-card-section>
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  }
 }
 </script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+</style>
