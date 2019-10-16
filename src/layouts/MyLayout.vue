4a4d07edd0f2e81ae456138c1fac93e257bdf4e1
mimeonline <michael.meierhoff@gmail.com>
2019-10-16 16:13:24 +0200

    Add Chrome as default Browser to open and Change main column to max width 500px.

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -2,22 +2,19 @@
   <q-layout view="hHh lpr fff">
     <q-header elevated class="bg-lime-6 text-grey-9">
       <q-toolbar>
-
-        <q-toolbar-title>
-          Cookma - The social Cooking App
-        </q-toolbar-title>
+        <q-toolbar-title>Cookma - The social Cooking App</q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
     <q-page-container>
       <div class="row">
-        <div class="col-md-4 bg-grey-3"></div>
-        <div class="col-md-4 bg-grey-3 self-center">
+        <div class="col-md bg-grey-3"></div>
+        <div class="col-md bg-grey-3 self-center" style="max-width: 500px;">
           <router-view />
         </div>
-      <div class="col-md-4 bg-grey-3"></div>
-    </div>
+        <div class="col-md bg-grey-3"></div>
+      </div>
     </q-page-container>
   </q-layout>
 </template>
