53e76997134b04f7db98590c506e042bbfd47a77
mimeonline <michael.meierhoff@gmail.com>
2019-10-28 21:48:26 +0100

    Add MyRecipe Page Contect

--- src/pages/recipe/MyRecipes.vue
+++ src/pages/recipe/MyRecipes.vue
@@ -1,6 +1,21 @@
 <template>
   <q-page padding>
-    Meine Rezepte
+        <div class="row justify-center q-gutter-lg q-pb-lg">
+          <div
+            class="col-3"
+            style=" max-width: 300px;"
+            v-for="card in 11"
+            :key="card"
+          >
+            <q-card>
+              <q-img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Spaghetti_bolognese_pasta.jpg/1600px-Spaghetti_bolognese_pasta.jpg"
+              />
+              <q-card-section>Mein Rezept</q-card-section>
+            </q-card>
+          </div>
+        </div>
+
   </q-page>
 </template>
 
@@ -9,3 +24,9 @@ export default {
   // name: 'PageName',
 }
 </script>
+
+<style>
+.c-card {
+  max-width: 250px;
+}
+</style>
