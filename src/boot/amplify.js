cbbe9d210bd815a80135fa556d5e6b7a5ba8e045
mimeonline <michael.meierhoff@gmail.com>
2019-11-01 17:18:59 +0100

    Add AWS Authentification

---
+++ src/boot/amplify.js
@@ -0,0 +1,11 @@
+import Amplify, { Logger, I18n } from '@aws-amplify/core'
+import Auth, { AuthClass } from '@aws-amplify/auth'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+
+import awsconfig from './aws-exports'
+
+Amplify.configure(awsconfig)
+
+export default ({ Vue }) => {
+  Vue.use(AmplifyPlugin, { Logger, I18n, Auth, AuthClass })
+}
--- src/pages/Imprint.vue
+++ src/pages/Imprint.vue
@@ -1,17 +1,60 @@
 <template>
   <q-page padding>
-    <div style="max-width: 800px; margin:0 auto; ">
+    <amplify-authenticator :authConfig="authConfig"></amplify-authenticator>
+<amplify-sign-out></amplify-sign-out>
+    <!-- <div style="max-width: 800px; margin:0 auto; ">
       <q-card>
         <q-card-section class="text-h4 text-bold text-center">
           Impressum
         </q-card-section>
       </q-card>
-    </div>
+    </div> -->
   </q-page>
 </template>
 
 <script>
+import { components } from 'aws-amplify-vue'
+
 export default {
-  // name: 'PageName',
+  data () {
+    return {
+      authConfig: {
+        signUpConfig: {
+          header: 'Willkomen bei Cookma! Erstelle Deinen Account',
+          hideAllDefaults: true,
+          defaultCountryCode: '49',
+          signUpFields: [
+            {
+              label: 'E-Mail',
+              key: 'username',
+              required: true,
+              displayOrder: 1,
+              type: 'string',
+              signUpWith: true
+            },
+            {
+              label: 'Passwort',
+              key: 'password',
+              required: true,
+              displayOrder: 2,
+              type: 'password'
+            }
+          ]
+        },
+        signInConfig: {
+          header: 'Willkommen zurück!',
+          username: 'E-Mail'
+        }
+      }
+    }
+  },
+
+  components: {
+    ...components
+  },
+  created () {
+    console.log(this.$Amplify)
+    this.$Amplify.I18n.setLanguage('de')
+  }
 }
 </script>
