103a53300f7ea3cfc739110677ce2d8657a6b457
Meierhoff, Michael <michael.meierhoff@immonet.de>
2019-11-16 12:34:43 +0100

    Connect Frontend with Backend

--- src/store/myrecipes/actions.js
+++ src/store/myrecipes/actions.js
@@ -1,5 +1,6 @@
 import { axios } from 'boot/axios'
 import { myRecipes } from '../../statics/data/myRecipes'
+import Amplify from '@aws-amplify/core'
 
 export function mockMyRecipes (context) {
   context.commit('setMyRecipes', myRecipes)
@@ -8,8 +9,9 @@ export function mockMyRecipes (context) {
 export function fetchMyRecipes (context) {
   (async () => {
     try {
-      // TODO URL have to configure in a central place and myRecipesId is temporaryly a fix value for prototyping
-      let response = await axios.get('http://localhost:8080/myrecipes/3b9f5040-295d-4885-905c-6774208d38bc')
+      var currentUser = await Amplify.Auth.currentAuthenticatedUser()
+      // TODO URL have to configure in a central place is temporaryly a fix value for prototyping
+      let response = await axios.get('http://localhost:8080/myrecipes/' + currentUser.attributes.sub)
       context.commit('setMyRecipes', response.data)
     } catch (error) {
       mockMyRecipes(context)
--- src/store/recipe/actions.js
+++ src/store/recipe/actions.js
@@ -1,52 +1,25 @@
 // import { LocalStorage, uid } from 'quasar'
 import { axios } from 'boot/axios'
-// import Amplify from '@aws-amplify/core'
+import Amplify from '@aws-amplify/core'
 
 import { recipe } from '../../statics/data/recipe'
-
-// async function postData (recipe) {
-//   let apiName = 'apie4be9e4f'
-//   let path = '/recipes'
-//   let myInit = {
-//     headers: {
-//       Authorization: `Bearer ${(await Amplify.Auth.currentSession()).getAccessToken().getJwtToken()}`,
-//       'Content-Type': 'application/json'
-//     },
-//     body: { ...recipe }
-//   }
-//   return Amplify.API.post(apiName, path, myInit)
-// }
 
 export function storeRecipe ({ getters, commit }) {
   (async () => {
     try {
-      commit('setUserId', '3b9f5040-295d-4885-905c-6774208d38bc')
+      var currentUser = await Amplify.Auth.currentAuthenticatedUser()
+      console.log(currentUser.attributes.sub)
+      commit('setUserId', currentUser.attributes.sub)
       commit('setImageId', 'statics/images/food-1932466_640.jpg')
       var recipeCreate = getters['recipeCreate']
       console.log(recipeCreate)
-      // TODO URL have to configure in a central place and myRecipesId is temporaryly a fix value for prototyping
+      // TODO URL have to configure in a central place is temporaryly a fix value for prototyping
       let response = await axios.post('http://localhost:8080/recipes', recipeCreate)
       console.log(response.data)
     } catch (error) {
       console.log(error)
     }
   })()
-  // (async () => {
-  //   var currentUser = await Amplify.Auth.currentAuthenticatedUser()
-  //   commit('setUserId', currentUser.attributes.sub)
-  // })()
-  // commit('setRecipeId', uid())
-  // console.log(new Date())
-  // // postData(getters.recipeCreate)
-  // postData(recipe)
-
-  // let recipes = LocalStorage.getItem('recipes')
-  // if (recipes) {
-  //   recipes.push(getters.recipeCreate)
-  //   LocalStorage.set('recipes', recipes)
-  // } else {
-  //   LocalStorage.set('recipes', [getters.recipeCreate])
-  // }
 }
 
 export function uploadRecipeImage (context, files) {
@@ -69,22 +42,10 @@ export function uploadRecipeImage (context, files) {
   })
 }
 
-// async function getRecipes () {
-//   let apiName = 'apie4be9e4f'
-//   let path = '/recipes'
-//   let myInit = {
-//     headers: {
-//       // 'Authorization': `Bearer ${(await this.$Amplify.Auth.currentSession()).getAccessToken().getJwtToken()}`,
-//       // 'Content-Type': 'application/json'
-//     }
-//   }
-//   return Amplify.API.get(apiName, path, myInit)
-// }
-
 export function fetchRecipe (context, recipeId) {
   (async () => {
     try {
-      // TODO URL have to configure in a central place and myRecipesId is temporaryly a fix value for prototyping
+      // TODO URL have to configure in a central place is temporaryly a fix value for prototyping
       let response = await axios.get('http://localhost:8080/recipes/' + recipeId)
       console.log(response.data)
       context.commit('setRecipe', response.data)
