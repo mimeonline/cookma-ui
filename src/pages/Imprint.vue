ef3334fc6f5d258257204fe09cb112cc320df378
Meierhoff, Michael <michael.meierhoff@immonet.de>
2019-11-17 17:25:05 +0100

    Revert Auth Test in Imprint

--- src/pages/Imprint.vue
+++ src/pages/Imprint.vue
@@ -1,64 +1,18 @@
 <template>
   <q-page padding>
-    <amplify-authenticator :authConfig="authConfig"></amplify-authenticator>
-<amplify-sign-out></amplify-sign-out>
-    <!-- <div style="max-width: 800px; margin:0 auto; ">
+
+    <div style="max-width: 800px; margin:0 auto; ">
       <q-card>
         <q-card-section class="text-h4 text-bold text-center">
           Impressum
         </q-card-section>
       </q-card>
-    </div> -->
+    </div>
   </q-page>
 </template>
 
 <script>
-import { components } from 'aws-amplify-vue'
-
 export default {
-  data () {
-    return {
-      authConfig: {
-        signUpConfig: {
-          header: 'Willkomen bei Cookma! Erstelle Deinen Account',
-          hideAllDefaults: true,
-          defaultCountryCode: '49',
-          signUpFields: [
-            {
-              label: 'E-Mail',
-              key: 'username',
-              required: true,
-              displayOrder: 1,
-              type: 'string',
-              signUpWith: true
-            },
-            {
-              label: 'Passwort',
-              key: 'password',
-              required: true,
-              displayOrder: 2,
-              type: 'password'
-            }
-          ]
-        },
-        signInConfig: {
-          header: 'Willkommen zurück!',
-          username: 'E-Mail'
-        }
-      }
-    }
-  },
 
-  components: {
-    ...components
-  },
-  created () {
-    this.$Amplify.Auth.currentAuthenticatedUser({
-      bypassCache: false // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-    }).then(user => console.log(user.attributes.sub))
-      .catch(err => console.log(err))
-    console.log(this.$Amplify.Auth)
-    this.$Amplify.I18n.setLanguage('de')
-  }
 }
 </script>
