ca87551dd89de08e89b56bea8d7be7b5a55fba21
Meierhoff, Michael <michael.meierhoff@immonet.de>
2019-11-30 10:59:20 +0100

    Add content for privacy pages

--- src/pages/Imprint.vue
+++ src/pages/Imprint.vue
@@ -1,10 +1,32 @@
 <template>
   <q-page padding>
-
     <div style="max-width: 800px; margin:0 auto; ">
       <q-card>
-        <q-card-section class="text-h4 text-bold text-center">
-          Impressum
+        <q-card-section class="q-pa-lg">
+          <div class="text-h3 text-bold q-pb-lg">Impressum</div>
+          <p>Angaben gemäß § 5 TMG</p>
+          <p>
+            Dipl. Ing. (FH) Michael Meierhoff<br />
+            Josephstraße 4<br />
+            22041 Hamburg <br />
+            Deutschland <br />
+          </p>
+          <p>
+            <strong>Kontakt:</strong><br />
+            Telefon: 040-60942187
+            E-Mail: <a href="mailto:hello@cookma.io">info@cookma.io</a><b />
+          </p>
+          <p>
+            <strong>Informationen zur Streitbeilegung</strong><br />
+            Die EU-Kommission stellt unter
+            <a href="http://ec.europa.eu/consumers/odr/"
+              >http://ec.europa.eu/consumers/odr/
+            </a>
+            eine Plattform zur Online-Streitbeilegung (OS) zur Verfügung. Wir
+            sind jedoch weder verpflichtet noch bereit, an
+            Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle
+            teilzunehmen.
+          </p>
         </q-card-section>
       </q-card>
     </div>
@@ -12,7 +34,5 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
--- src/pages/UserCondition.vue
+++ src/pages/UserCondition.vue
@@ -2,8 +2,24 @@
   <q-page padding>
     <div style="max-width: 800px; margin:0 auto; ">
       <q-card>
-        <q-card-section class="text-h4 text-bold text-center">
-          Nutzerbedingungen
+        <q-card-section class="q-pa-lg">
+          <div class="text-h3 text-bold q-pb-lg">Nutzerbedingungen</div>
+          <p>
+            <strong>Urheberrecht</strong><br /><br />
+            Die durch die Seitenbetreiber erstellten Inhalte und Werke auf
+            diesen Seiten unterliegen dem deutschen Urheberrecht. Die
+            Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+            Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der
+            schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+            Downloads und Kopien dieser Seite sind nur für den privaten, nicht
+            kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser
+            Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte
+            Dritter beachtet. Insbesondere werden Inhalte Dritter als solche
+            gekennzeichnet. Sollten Sie trotzdem auf eine
+            Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+            entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
+            werden wir derartige Inhalte umgehend entfernen.
+          </p>
         </q-card-section>
       </q-card>
     </div>
